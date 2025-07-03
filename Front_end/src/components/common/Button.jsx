b8338118b961e1b0556404eca59b1d7c467ad6b3
Kang0408 <trandanhkhang482004@gmail.com>
2025-07-03 14:43:39 +0700

    feat(ui): add button component

---
+++ Front_end/src/components/common/Button.jsx
@@ -0,0 +1,36 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+export default function Button({
+  children = "Button",
+  color = "default",
+  handleClick,
+  type = "button",
+  icon,
+  ...rest
+}) {
+  return (
+    <>
+      <button
+        className={`${
+          color == "primary"
+            ? "bg-primary-500 text-white"
+            : "bg-gray-400 text-white"
+        } px-4 py-2 shadow-lg rounded-md hover:opacity-80 transition-all`}
+        onClick={handleClick}
+        type={type}
+        {...rest}
+      >
+        <div className="flex items-center gap-2">
+          {icon ? (
+            <>
+              <Icon icon={icon} className="text-xl" />
+            </>
+          ) : (
+            ""
+          )}
+          {children}
+        </div>
+      </button>
+    </>
+  );
+}
